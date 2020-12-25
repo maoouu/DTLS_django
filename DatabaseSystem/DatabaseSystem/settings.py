2ec899fb6bfd739bc8dd45ccff677dc237b31a09
maoouu <dale.reyes@outlook.ph>
2020-12-25 21:25:20 +0800

    screenshot (v1.0_12.25.20)
    Go to this whenever you need to revert back.

--- DatabaseSystem/DatabaseSystem/settings.py
+++ DatabaseSystem/DatabaseSystem/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = 'DatabaseSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoweb',
+        'USERNAME': 'Admin',
+        'PASSWORD': 'xrm600700',
+        'PORT': '3306',
+        'HOST': 'localhost'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
